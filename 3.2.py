e5e0d5cfbd8afcd73eb7a3bb2cfa12abd0acbc09
viswak <63440120+viswakbaddie@users.noreply.github.com>
2024-05-07 16:59:26 +0300

    Update 3.2.py

--- 3.2.py
+++ 3.2.py
@@ -1,120 +1,84 @@
-import time
-from ssd1306 import SSD1306_I2C
-from machine import UART, Pin, I2C
-from filefifo import Filefifo
+from machine import Pin, I2C
 from fifo import Fifo
-from led import Led 
+from ssd1306 import SSD1306_I2C
+from time import ticks_ms 
+
 
 class Encoder:
-    def __init__(self, rot_a, rot_b):
-        self.a = Pin(rot_a, mode = Pin.IN, pull = Pin.PULL_UP)
-        self.b = Pin(rot_b, mode = Pin.IN, pull = Pin.PULL_UP)
-        self.fifo = Fifo(30, typecode = 'i')
-        self.a.irq(handler = self.handler, trigger = Pin.IRQ_RISING, hard = True)
+    def __init__(self, rot_a, rot_b, btn):
+       self.a = Pin(rot_a, mode = Pin.IN, pull = Pin.PULL_UP)
+       self.b = Pin(rot_b, mode = Pin.IN, pull = Pin.PULL_UP)
+       self.btn = Pin(btn, mode = Pin.IN, pull = Pin.PULL_UP)
+       self.prev_tick = 0
+       
+       self.fifo = Fifo(30, typecode = 'i')
+       self.a.irq(handler = self.handler, trigger = Pin.IRQ_RISING, hard = True)
+       self.btn.irq(handler = self.btn_handler, trigger = Pin.IRQ_RISING, hard = True)
+            
+            
+    def btn_handler(self, pin):
+        curr_tick = ticks_ms()
+    
+        if curr_tick - self.prev_tick > 50:
+            self.fifo.put(2)
         
-    def handler(self, pin):
+        self.prev_tick = curr_tick
+        
+        
+    def handler(self, pin):       
         if self.b():
             self.fifo.put(-1)
+        
         else:
-            self.fifo.put(+1)
-            
-rot = Encoder(10,11)
-led1 = Led(22, Pin.OUT)
-led2 = Led(21, Pin.OUT)
-led3 = Led(20, Pin.OUT)
-brightness = 50
-pts = 0
-
-
-press = Fifo(20, typecode = 'i')
-rot_butt = Pin(12, Pin.IN, pull = Pin.PULL_UP)
-
-def button_handler(pin):
-    global ts
-    global pts
-    ts = time.ticks_ms()
-    press.put(1)
-    
-rot_butt.irq(handler = button_handler, trigger = Pin.IRQ_FALLING, hard = True)
-
-
-i2c = I2C(1, scl = Pin(15), sda = Pin(14), freq = 400_000)
-oled_width = 128
-oled_height = 64
-oled = SSD1306_I2C(oled_width, oled_height, i2c)
-
-
-text_width = 8
-text_height = 8
+            self.fifo.put(1)
+                
+                
+i2c  = I2C(1, scl = Pin(15), sda = Pin(14), freq = 400000)
+oled = SSD1306_I2C(128, 64, i2c)
 oled.fill(0)
-text_pos_magn = [0,2,4]
-oled.text("LED1 - OFF", 0, 0 + 1, 1)
-oled.text("LED2 - OFF", 0, (text_height * text_pos_magn[1]) + 1, 1)
-oled.text("LED3 - OFF", 0, (text_height * text_pos_magn[2]) + 1, 1)
-oled.show()
-highlighted_text = 0
-highlight = False
-tick_list = []
 
+rot = Encoder(10, 11, 12)
 
+leds = [Pin(22, Pin.OUT), Pin(21, Pin.OUT), Pin(20, Pin.OUT)]
+led_states  = [led.value() for led in leds]
 
-while True:
-    if highlighted_text != 0:
-        pos = text_pos_magn[highlighted_text - 1]
-        oled.rect(0, text_height * pos, oled_width, text_height + 2, 1)
+led_hovers  = [1, 0, 0]
+hovered_led = 0
 
+def show_text(states, hovers):
+    oled.fill(0)
     
-
-    
+    for i in range(3):
+        text = f"{'<' if hovers[i] else ' '}LED{i + 1}-{'ON' if states[i] else 'OFF'}{'>' if hovers[i] else ' '}"
+        oled.text(text, 25, 25 * i, 1)
+        
     oled.show()
     
-    while rot.fifo.has_data():
-        value = rot.fifo.get()
-        if value == 1:
-            if highlighted_text + 1 < 4:
-                pos = text_pos_magn[highlighted_text - 1]
-                oled.rect(0, text_height * pos, oled_width, text_height + 2, 0)
-                highlighted_text += 1
 
-        elif value == -1:
-            if highlighted_text > 1:
-                pos = text_pos_magn[highlighted_text - 1]
-                oled.rect(0, text_height * pos, oled_width, text_height + 2, 0)
-                highlighted_text -= 1
+show_text(led_states, led_hovers)
+while True:
+    if rot.fifo.has_data():
+        action = rot.fifo.get()
+        
+        if action == 2:
+            led = leds[hovered_led]
+            led.toggle()
+            
+            led_states[hovered_led] = led.value()
+            
+        
+        elif action == 1:
+            if not led_hovers[2]:
+                led_hovers[hovered_led] = 0
+                hovered_led += 1
+                led_hovers[hovered_led] = 1
                 
         
-    while press.has_data():
-        val = press.get()
-        if pts != 0:
-            if ts - pts <= 250:
-                continue
-        
+        elif action == -1:
+            if not led_hovers[0]:
+                led_hovers[hovered_led] = 0
+                hovered_led -= 1
+                led_hovers[hovered_led] = 1
+                
+        show_text(led_states, led_hovers)
 
-        if highlighted_text == 1:
-            led1.toggle()
-        elif highlighted_text == 2:
-            led2.toggle()
-        elif highlighted_text == 3:
-            led3.toggle()
-            pts = ts
-        
-        if led1.value():
-            oled.rect(0, 0 + 1, oled_width, text_height, 0, [True])
-            oled.text("LED1 - ON", 0, 0 + 1, 1)
-        else:
-            oled.rect(0, 0 + 1, oled_width, text_height, 0, [True])
-            oled.text("LED1 - OFF",0, 0 + 1, 1)
-        
-        if led2.value():
-            oled.rect(0, (text_height * text_pos_magn[1]), oled_width, text_height, 0, [True])
-            oled.text("LED2 - ON", 0, (text_height * text_pos_magn[1]) + 1, 1)
-        else:
-            oled.rect(0, (text_height * text_pos_magn[1]), oled_width, text_height, 0, [True])
-            oled.text("LED2 - OFF", 0, (text_height * text_pos_magn[1]) + 1, 1)
-        
-        if led3.value():
-            oled.rect(0, (text_height * text_pos_magn[2]), oled_width, text_height, 0, [True])
-            oled.text("LED3 - ON", 0, (text_height * text_pos_magn[2]) + 1, 1)
-        else:
-            oled.rect(0, (text_height * text_pos_magn[2]), oled_width, text_height, 0, [True])
-            oled.text("LED3 - OFF", 0, (text_height * text_pos_magn[2]) + 1, 1)3.
